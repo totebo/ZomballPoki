5a5988ad0c3e943bc6675e2492c1e05a7617900d
Niclas Aberg <niclas@totebo.com>
2021-04-10 14:59:07 +0100

    Build works.

--- gidloader.js
+++ gidloader.js
@@ -176,7 +176,7 @@ if (hasFBInstant) {
 	FBInstant.initializeAsync();
 }
 var progressCur=0;
-var progressMax=3259109;
+var progressMax=6830281;
 function downloadProgress(diff) {
 	progressCur+=diff;
 	var pro=100*progressCur/progressMax;
@@ -200,7 +200,7 @@ function getParameterByName(name, url) {
             return decodeURIComponent(results[2].replace(/\+/g," "));
      }      
   var GAPP_URL=null;
-  //GAPP_URL="gideros.GApp";
+  GAPP_URL="Zomball.GApp.gidz";
   var uarg=getParameterByName("GIDEROS_GAPP");
   if ((uarg!=null) && (uarg!=""))
     GAPP_URL=uarg;
@@ -225,11 +225,11 @@ function getParameterByName(name, url) {
     preInit: [],
     TOTAL_MEMORY: GIDEROS_MEMORY_MB*1024*1024,
     GiderosPlugins: [   /*GIDEROS_DYNLIB_PLUGIN*/
-"OggCore.wasm",
-"OggVorbis.wasm",
-"OggOpus.wasm",
-"json.wasm",
-"liquidfun.wasm", null ],
+"liquidfun.gidz",
+"json.gidz",
+"OggCore.gidz",
+"OggVorbis.gidz",
+"OggOpus.gidz", null ],
     JSPlugins: [ /*GIDEROS_JS_PLUGIN*/ null ],
     requestFile: function (file) { return 0; },
     print: (function() {
