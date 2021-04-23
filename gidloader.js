e62a71aabe224db46a9fbee02bab677084849387
Niclas Aberg <niclas@totebo.com>
2021-04-23 12:09:46 +0100

    The game throws two consecutive gameplayStart() events when the player shoots for the first time:

--- gidloader.js
+++ gidloader.js
@@ -176,7 +176,7 @@ if (hasFBInstant) {
 	FBInstant.initializeAsync();
 }
 var progressCur=0;
-var progressMax=6832553;
+var progressMax=6832311;
 function downloadProgress(diff) {
 	progressCur+=diff;
 	var pro=100*progressCur/progressMax;
@@ -225,11 +225,11 @@ function getParameterByName(name, url) {
     preInit: [],
     TOTAL_MEMORY: GIDEROS_MEMORY_MB*1024*1024,
     GiderosPlugins: [   /*GIDEROS_DYNLIB_PLUGIN*/
+"liquidfun.gidz",
 "OggCore.gidz",
 "OggVorbis.gidz",
 "OggOpus.gidz",
-"json.gidz",
-"liquidfun.gidz", null ],
+"json.gidz", null ],
     JSPlugins: [ /*GIDEROS_JS_PLUGIN*/ null ],
     requestFile: function (file) { return 0; },
     print: (function() {
